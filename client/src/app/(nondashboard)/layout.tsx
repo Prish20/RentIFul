02440b1d1d25b9bf58c524d3f05672fc8e12320e
Prish20 <contact@christadriansanya.me>
2025-03-04 10:59:43 +0300

    Refactor Navbar component to include user authentication and role-based actions; adjust NAVBAR_HEIGHT constant

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -2,12 +2,9 @@
 
 import Navbar from "@/components/Navbar"
 import { NAVBAR_HEIGHT } from "@/lib/constants"
-import { useGetAuthUserQuery } from "@/state/api";
 
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    const { data: authUser } = useGetAuthUserQuery();
-    console.log("authUser", authUser);
     return (
         <div className="h-full w-full">
             <Navbar />

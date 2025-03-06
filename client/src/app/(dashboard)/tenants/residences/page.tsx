55c8b7a46e5fdbd31ea63aa5f2c78d0ae34e934d
Prish20 <contact@christadriansanya.me>
2025-03-07 02:49:38 +0300

    Add Header and Loading components; implement residences, favorites, and applications pages with enhanced user experience

--- client/src/app/(dashboard)/tenants/residences/page.tsx
+++ client/src/app/(dashboard)/tenants/residences/page.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+"use client";
+
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import {
+    useGetAuthUserQuery,
+    useGetCurrentResidencesQuery,
+    useGetTenantQuery,
+} from "@/state/api";
+import React from "react";
 
 const Residences = () => {
-    return (
-        <div>Residences</div>
-    )
-}
+    const { data: authUser } = useGetAuthUserQuery();
+    const { data: tenant } = useGetTenantQuery(
+        authUser?.cognitoInfo?.userId || "",
+        {
+            skip: !authUser?.cognitoInfo?.userId,
+        }
+    );
 
-export default Residences
+    const {
+        data: currentResidences,
+        isLoading,
+        error,
+    } = useGetCurrentResidencesQuery(authUser?.cognitoInfo?.userId || "", {
+        skip: !authUser?.cognitoInfo?.userId,
+    });
+
+    if (isLoading) return <Loading />;
+    if (error) return <div>Error loading current residences</div>;
+
+    return (
+        <div className="dashboard-container mx-4 md:mx-20 mt-10 bg-primary-700/20 rounded-xl">
+            <Header
+                title="Current Residences"
+                subtitle="View and manage your current living spaces"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {currentResidences?.map((property) => (
+                    <Card
+                        key={property.id}
+                        property={property}
+                        isFavorite={tenant?.favorites.includes(property.id) || false}
+                        onFavoriteToggle={() => { }}
+                        showFavoriteButton={false}
+                        propertyLink={`/tenants/residences/${property.id}`}
+                    />
+                ))}
+            </div>
+            {(!currentResidences || currentResidences.length === 0) && (
+                <p>You don&lsquo;t have any current residences</p>
+            )}
+        </div>
+    );
+};
+
+export default Residences;
